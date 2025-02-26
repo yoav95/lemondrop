5a4061c00c7b637e30df919a2976e93bf38eb972
Yoav Amenou <yoav.amenou@gmail.com>
2025-02-26 20:37:45 +0200

    fine

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,17 @@
 import styles from "./Navbar.module.css";
 // import Link from "./Link";
 import { NavLink, Link, useLocation  } from "react-router-dom";
+import { ReactComponent as ReactLogo } from './assets/logop.svg'
+
 
 import Wrapper from "./Wrapper";
 import { FaInstagram  } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { useEffect, useState } from "react";
 import SoundPlayer from './SoundPlayer'; // Import your sound player component
-import { TfiMenu } from "react-icons/tfi";function Navbar() {
+import { TfiMenu } from "react-icons/tfi";
+
+function Navbar() {
   const [isBlinking, setIsBlinking] = useState(false);
   const [playSound, setPlaySound] = useState(false);
   const [showMenu, setShowMenu] = useState(false)
@@ -25,16 +29,17 @@ import { TfiMenu } from "react-icons/tfi";function Navbar() {
     setIsBlinking(false);
   };
   const toggleMenu  = () => {
+    
     setShowMenu((prev) => !prev)
 
   }
 
   useEffect(() => {
-    // Set interval to start blinking every 5 seconds
+
     const interval = setInterval(() => {
-      // startBlinking();
-      // setTimeout(stopBlinking, 2500); 
-    }, 10000); // Repeat every 5 seconds
+      startBlinking();
+      setTimeout(stopBlinking, 2500); 
+    }, 5000); // Repeat every 5 seconds
 
     // Clean up interval on component unmount
     return () => clearInterval(interval);
@@ -43,17 +48,18 @@ import { TfiMenu } from "react-icons/tfi";function Navbar() {
     return (
       <>
       <Wrapper>
-        <header className={`${styles.header} ${isBlinking ? styles.blinking : ''}`}>
+        
+        <header className={`${styles.header}`}>
             
-          <div  className={`${styles.logo} ${isBlinking ? styles.popping : ''}`}>< Link to="/"><img onClick={triggerSound} src="/logop.svg" /></Link></div>
+          <div  className={`${styles.logo}`}>< Link to="/"><ReactLogo className={isBlinking ? `${styles.svg}` : `${styles.svg} ${styles.popping}`} id="svg"/></Link></div>
           <div className={styles.box}>
           <ul className={showMenu ? `${styles.ul} ${styles.show}` : styles.ul} onClick={toggleMenu}>
           
           <li><a onClick={triggerSound}  href="https://www.instagram.com/madebylemondrop/" target="_blank" ><div className={styles.icon}><FaInstagram size={'20px'}/></div></a></li>
-              <li onClick={triggerSound} className={`${styles.li} ${location.pathname === "/contact" ? styles.active : ""}`}><NavLink   to="/contact" color="#2A4F6B">יצירת קשר</NavLink></li>
-              <li onClick={triggerSound} className={`${styles.li} ${location.pathname === "/custom" ? styles.active : ""}`}><NavLink to="/custom" color="#2A4F6B">תיקים בהתאמה אישית</NavLink></li>
-              <li onClick={triggerSound} className={`${styles.li} ${location.pathname === "/info" ? styles.active : ""}`}><NavLink   to="/info" color="#2A4F6B">מידע</NavLink></li>
-              <li onClick={triggerSound} className={`${styles.li} ${location.pathname === "/shop" ? styles.active : ""}`}><NavLink  to="/shop" color="#2A4F6B">חנות</NavLink></li>
+              <li onClick={triggerSound} className={`${styles.li}`}><NavLink   to="/contact" color="#2A4F6B">יצירת קשר</NavLink></li>
+              <li onClick={triggerSound} className={`${styles.li}`}><NavLink to="/custom" color="#2A4F6B">תיקים בהתאמה אישית</NavLink></li>
+              <li onClick={triggerSound} className={`${styles.li}`}><NavLink   to="/info" color="#2A4F6B">מידע</NavLink></li>
+              <li onClick={triggerSound} className={`${styles.li}`}><NavLink  to="/shop" color="#2A4F6B">חנות</NavLink></li>
               <li onClick={triggerSound} ><NavLink to="/cart"><div className={styles.cart}><MdOutlineShoppingCart size='25px' /><p>1</p></div></NavLink></li>
               
           </ul>
